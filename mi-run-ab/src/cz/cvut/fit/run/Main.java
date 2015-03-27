3a1ead0361a84476b54b4cc6b5179103b3470583
apelttom <apelttom@gmail.com>
2015-03-27 17:41:04 +0100

    NEW: Method for displaying tokens in AST

--- mi-run-ab/src/cz/cvut/fit/run/Main.java
+++ mi-run-ab/src/cz/cvut/fit/run/Main.java
@@ -1,14 +1,25 @@
 package cz.cvut.fit.run;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.security.Principal;
 
+import antlr.CommonAST;
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
+import antlr.TreeParser;
+import antlr.collections.AST;
 import cz.cvut.fit.run.parser.JavaLexer;
 import cz.cvut.fit.run.parser.JavaRecognizer;
+import cz.cvut.fit.run.parser.JavaTreeParser;
 
 public class Main {
 
@@ -27,15 +38,57 @@ public class Main {
 
 			// start parsing at the compilationUnit rule
 			parser.compilationUnit();
-
-			// here we have full AST
-			System.out.println(parser.getAST().toStringTree());
-
+			
+			// get AST tree and print it in LISP notation
+			CommonAST myTree = (CommonAST) parser.getAST();
+//			System.out.println(myTree.toStringList());
+			
+//			printCommonAST(myTree);
+			
+			printRoot(myTree);
+			
+			
 			// generate bytecode
 
 		} catch (FileNotFoundException | RecognitionException | TokenStreamException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Writes xml form of AST into a file given a specific path
+	 * 
+	 * @param t
+	 *            AST writen into the file
+	 * @throws IOException
+	 *             if the file exists but is a directory rather than a regular
+	 *             file, does not exist but cannot be created, or cannot be
+	 *             opened for any other reason
+	 */
+/*
+	static void printCommonAST(CommonAST t) throws IOException{
+		File file = new File("path/Filename.xml");
+		Writer w = new FileWriter(file);
+		t.xmlSerialize(w);
+		w.flush();
+	}
+*/
+	
+	/**
+	 * Prints the AST into console. Recursion method.
+	 * 
+	 * @param node
+	 *            root of the tree which should be printed
+	 */
+	static void printRoot(AST node) {
+		if (node == null) {
+			return;
+		}
+		System.out.println(node.toString());
+		printRoot(node.getFirstChild());
+		printRoot(node.getNextSibling());
 	}
 
 }
