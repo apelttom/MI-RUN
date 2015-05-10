689a53827c98b195e5fbd80190a9d16d06b253c5
bicanjir <bicanjir@fit.cvut.cz>
2015-05-10 12:55:02 +0200

    FieldInfo

---
+++ mi-run-ab/src/cz/cvut/fit/run/compiler/FieldInfo.java
@@ -0,0 +1,29 @@
+package cz.cvut.fit.run.compiler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FieldInfo {
+	public List<String> flags;
+	public String name;
+	public String type;
+	public FieldInfo(String name, String type) {
+		super();
+		this.name = name;
+		this.type = type;
+		flags = new ArrayList<String>(1);
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		if (!flags.isEmpty()) {
+			builder.append(flags);
+			builder.append(" ");
+		}
+		builder.append(type);
+		builder.append(" ");
+		builder.append(name);
+		builder.append(";\n");
+		return builder.toString();
+	}
+}
